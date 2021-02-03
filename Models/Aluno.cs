41002a309fe1d702cd77e1700651008d244c037d
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-02-03 19:01:26 -0300

    iniciando um projeto mvc

---
+++ Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaGerenciadorDisciplinas.Models
+{
+    public class Aluno
+    {
+    }
+}
