021dd7708d9ab6bfd0281fe2ca6b3bd763fc90a2
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-02-05 22:22:38 -0300

    inserindo mais validações em aluno

--- src/Models/Aluno.cs
+++ src/Models/Aluno.cs
@@ -12,28 +12,41 @@ namespace SisGerenciador.Models
         public int Id { get; set; }
 
         [Display(Name = "CPF")]
+        [StringLength(11)]
         [Required(ErrorMessage = "CPF é obrigatório", AllowEmptyStrings = false)]
         public string CPF { get; set; }
 
         [Display(Name = "Nome")]
+        [StringLength(500)]
         [Required(ErrorMessage = "Nome é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
 
         [Display(Name = "Email")]
-        [Required(ErrorMessage = "Email é obrigatório", AllowEmptyStrings = false)]
+        [StringLength(500)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Display(Name = "Telefone")]
+        [StringLength(15)]
+        [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
 
+        [Display(Name = "Endereço")]
+        [StringLength(500)]
         public string Endereco { get; set; }
 
-        [Display(Name = "Data/Hora Inicio")]
+        [Display(Name = "Data de Nascimento")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
-        [Required(ErrorMessage = "Data/Hora do inicio é obrigatória", AllowEmptyStrings = false)]
+        [Required(ErrorMessage = "Data de nascimento é obrigatória", AllowEmptyStrings = false)]
         public DateTime DtNasc { get; set; }
 
-        public string Matricula { get; set; }
+        [Display(Name = "Matricula")]
+        [Required(ErrorMessage = "Matricula é obrigatória", AllowEmptyStrings = false)]
+        public int Matricula { get; set; }
 
+        [Display(Name = "Data de Matricula")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        [Required(ErrorMessage = "Data de matricula é obrigatória", AllowEmptyStrings = false)]
         public DateTime DtMatricula { get; set; }
 
         public Instituicao Instituicao { get; set; }
