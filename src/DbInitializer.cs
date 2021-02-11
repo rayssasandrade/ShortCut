18e6a9553ec2798efe5e8a0e2c1c544aa57b290f
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-02-11 11:55:48 -0300

    Criado o usuário

---
+++ src/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisGerenciador
+{
+    public class DbInitializer
+    {
+    }
+}
---
+++ src/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SisGerenciador.src.Models
+{
+    public class Usuario : IdentityUser
+    {
+        [NotMapped]
+        public bool IsAdmin { get; set; }
+
+        [Required(ErrorMessage = "Nome do usuario é obrigatorio.")]
+        [MinLength(6, ErrorMessage = "Nome inválido")]
+        public string Nome { get; set; }
+    }
+}
